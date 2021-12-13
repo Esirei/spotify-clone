52272f57918b14585dc23e67eecfd3e865344976
Esi're <esiri92@gmail.com>
2021-12-13 21:49:10 +0100

    add Sidebar component.

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,51 @@
+import {
+  HeartIcon,
+  HomeIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  RssIcon,
+  SearchIcon,
+} from '@heroicons/react/outline';
+import { FC } from 'react';
+
+const Sidebar: FC = () => {
+  return (
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <span>Home</span>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <span>Search</span>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <span>Your Library</span>
+        </button>
+
+        <hr className="border-gray-900" />
+
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
+          <span>Create Playlist</span>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
+          <span>Liked Songs</span>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
+          <span>Your Episodes</span>
+        </button>
+
+        <hr className="border-gray-900" />
+
+        {/*Render playlist*/}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
