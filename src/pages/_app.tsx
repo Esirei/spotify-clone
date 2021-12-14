ab881a5204e3f165ff50e49794da57352b4233e6
Esi're <esiri92@gmail.com>
2021-12-14 02:24:42 +0100

    wrap app with SessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
+import { SessionProvider, SessionProviderProps } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import { FC } from 'react';
 import '~/styles/globals.css';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+interface Props extends AppProps {
+  pageProps: SessionProviderProps;
+}
+
+const MyApp: FC<Props> = ({ Component, pageProps }) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default MyApp;
