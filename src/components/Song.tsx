961142d7ada274bba538d65a3acb162b58308299
Esi're <esiri92@gmail.com>
2021-12-18 19:00:09 +0100

    style song on hover

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -13,18 +13,20 @@ const Song: FC<Props> = ({ item: { track, added_at }, order }) => {
   const image = track.album.images.reduce((acc, curr) => (curr.width < acc.width ? curr : acc)).url;
 
   return (
-    <div className="grid grid-cols-4 space-x-4 md:grid-cols-6 lg:grid-cols-8">
+    <div className="grid grid-cols-4 px-5 py-1.5 space-x-4 md:grid-cols-6 lg:grid-cols-8 group hover:bg-white/10 rounded">
       <div className="flex col-span-3 items-center space-x-4">
         <p>{order + 1}</p>
         <img className="w-10 h-10" src={image} alt={track.album.name} />
         <div className="truncate">
           <p className="text-white truncate">{track.name}</p>
-          <p className="truncate">{track.artists.map(artist => artist.name).join(', ')}</p>
+          <p className="truncate group-hover:text-white">
+            {track.artists.map(artist => artist.name).join(', ')}
+          </p>
         </div>
       </div>
 
       <div className="hidden col-span-2 md:flex md:items-center">
-        <p className="truncate">{track.album.name}</p>
+        <p className="truncate group-hover:text-white">{track.album.name}</p>
       </div>
 
       <div className="hidden col-span-2 lg:flex lg:items-center">
