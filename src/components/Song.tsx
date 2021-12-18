1b6953654c2c018390b695b903cbc75f0727eea9
Esi're <esiri92@gmail.com>
2021-12-18 18:31:14 +0100

    increase width of song's title, album and added columns. truncate text

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -13,21 +13,21 @@ const Song: FC<Props> = ({ item: { track, added_at }, order }) => {
   const image = track.album.images.reduce((acc, curr) => (curr.width < acc.width ? curr : acc)).url;
 
   return (
-    <div className="grid grid-cols-3 space-x-4 md:grid-cols-4 lg:grid-cols-5">
-      <div className="flex col-span-2 items-center space-x-4">
+    <div className="grid grid-cols-4 space-x-4 md:grid-cols-6 lg:grid-cols-8">
+      <div className="flex col-span-3 items-center space-x-4">
         <p>{order + 1}</p>
         <img className="w-10 h-10" src={image} alt={track.album.name} />
         <div>
-          <p className="text-white">{track.name}</p>
-          <p>{track.artists.map(artist => artist.name).join(', ')}</p>
+          <p className="text-white truncate">{track.name}</p>
+          <p className="truncate">{track.artists.map(artist => artist.name).join(', ')}</p>
         </div>
       </div>
 
-      <div className="hidden md:flex md:items-center">
-        <p>{track.album.name}</p>
+      <div className="hidden col-span-2 md:flex md:items-center">
+        <p className="truncate">{track.album.name}</p>
       </div>
 
-      <div className="hidden lg:flex lg:items-center">
+      <div className="hidden col-span-2 lg:flex lg:items-center">
         <TimeAgo datetime={added_at} live />
       </div>
 
