236c360724535077f76945f24e7831a0f5f0032e
Esi're <esiri92@gmail.com>
2022-03-06 23:35:50 +0100

    Use eslint-config-esire

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  bracketSpacing: true,
-  bracketSameLine: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-  endOfLine: 'auto',
-  printWidth: 100,
-};
